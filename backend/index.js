f92dd2ed742a1e30631f9972ba1596cc4e63cf25
Akshit Vig <akshitvig48@gmail.com>
2024-12-01 01:03:47 +0530

    dsd

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,27 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import userRoute from './routes/user.js';
-import postRoute from './routes/post.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRoute from "./routes/user.js";
+import postRoute from "./routes/post.js";
 
-import './models/User.js';
-
-
+import "./models/User.js";
 
 dotenv.config();
 
 const app = express();
 
-
 app.use(express.json());
 
-app.use(cors(*));
+app.use(cors("*"));
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI);
 
-app.use('/api/user',userRoute);
-app.use('/api/post',postRoute);
+app.use("/api/user", userRoute);
+app.use("/api/post", postRoute);
 
-
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
