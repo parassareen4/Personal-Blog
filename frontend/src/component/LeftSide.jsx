7ddaa92133c7ba671990755da5a2e1277fe81093
parassareen4 <kyakrnahaibro@gmail.com>
2024-12-02 15:06:45 -0500

    fixed

--- frontend/src/component/LeftSide.jsx
+++ frontend/src/component/LeftSide.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AllPosts from "./Allposts.jsx";
+import { GetProfile } from './profilerete';
+import Createpost from './createpost';
 
 function LeftSide() {
   const navigate = useNavigate();
@@ -12,13 +14,13 @@ function LeftSide() {
         <div className="leftbuttons">
           {/* Use a function in onClick */}
           <div onClick={() => setContent(<AllPosts />)}>Home</div>
-          <div onClick={() => setContent(<div>Profile Page Coming Soon...</div>)}>Profile</div>
+          <div onClick={() => setContent(<GetProfile />)}>Profile</div>
           <div onClick={() => setContent(<div>Settings Page Coming Soon...</div>)}>Settings</div>
           <div onClick={() => {
             localStorage.removeItem("token"); // Handle logout logic
             navigate("/login");
           }}>Logout</div>
-          <div onClick={() => setContent(<div>Create Post Page Coming Soon...</div>)}>Create Post</div>
+          <div onClick={() => setContent(<Createpost />)}>Create Post</div>
         </div>
       </div>
       
