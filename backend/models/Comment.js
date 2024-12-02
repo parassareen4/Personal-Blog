25ba37f840b5e8490eca0f7e07634ba139902791
parassareen4 <kyakrnahaibro@gmail.com>
2024-12-01 19:29:43 -0500

    fixed

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -16,6 +16,6 @@ const commentSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('Comment', commentSchema);
+const Comment = mongoose.model('Comment', commentSchema);
 
-export default commentSchema;
+export default Comment;
