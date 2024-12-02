fcdc4ea01e7db1b34065aa98e86b22f13901fe4a
parassareen4 <kyakrnahaibro@gmail.com>
2024-12-02 13:38:17 -0500

    fixed

---
+++ frontend/src/component/LeftSide.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function LeftSide() {
+  return (
+    <div className="leftside">
+        <div>Home</div>
+        <div>Profile</div>
+        <div>Settings</div>
+        <div>Logout</div>
+        <div>Create Post</div>
+    </div>
+  )
+}
+
+export default LeftSide
