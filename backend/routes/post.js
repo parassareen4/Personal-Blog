c69275a4f8894b2e781e4fecada0463f92cfc318
parassareen4 <kyakrnahaibro@gmail.com>
2024-12-01 20:51:03 -0500

    fixed

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -5,7 +5,7 @@ import authentication from '../middlewares/auth.js';
 const router = express.Router();
 
 router.get('/all',authentication,async(req,res)=>{
-    const posts = await Post.find({author:req.user._id}).populate('author');
+    const posts = await Post.find({author:req.user._id}).populate('author','comments');
     res.json(posts)
 })
 
@@ -41,7 +41,7 @@ router.put('/update/:id',authentication,async(req,res)=>{
 router.get('/:id',authentication,async(req,res)=>{
     const {id} = req.params;
     try{
-        const post = await Post.findById(id).populate('author');
+        const post = await Post.findById(id).populate('author','comments');
         if(!post){
             res.status(404).json({message:'Post not found'})
         }
