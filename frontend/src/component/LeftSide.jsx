ca2abc09ff30615a39d36f0078d88371a7f12090
nitish <nitishbakshi05@gmail.com>
2024-12-04 01:43:29 +0530

    leftside edited

--- frontend/src/component/LeftSide.jsx
+++ frontend/src/component/LeftSide.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from 'react';
-import {  useNavigate } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import AllPosts from "./Allposts.jsx";
-import { GetProfile } from './profilerete';
-import Createpost from './createpost';
+import { GetProfile } from "./profilerete";
+import Createpost from "./createpost";
 
 function LeftSide() {
   const navigate = useNavigate();
@@ -16,21 +16,32 @@ function LeftSide() {
     <div>
       <div className="leftside">
         <div className="leftbuttons">
-          <div  onClick={() =>{ 
-            setContent(<AllPosts />)}}>Home</div>
+          <div
+            onClick={() => {
+              setContent(<AllPosts />);
+            }}
+          >
+            Home
+          </div>
           <div onClick={() => setContent(<GetProfile />)}>Profile</div>
-          <div onClick={() => setContent(<div>Settings Page Coming Soon...</div>)}>Settings</div>
-          <div onClick={() => {
-            localStorage.removeItem("token"); 
-            navigate("/signin");
-          }}>Logout</div>
+          <div
+            onClick={() => setContent(<div>Settings Page Coming Soon...</div>)}
+          >
+            Settings
+          </div>
+          <div
+            onClick={() => {
+              localStorage.removeItem("token");
+              navigate("/signin");
+            }}
+          >
+            Logout
+          </div>
           <div onClick={() => setContent(<Createpost />)}>Create Post</div>
         </div>
       </div>
-      
-      <div className="content">
-        {content}
-      </div>
+
+      <div className="content">{content}</div>
     </div>
   );
 }
